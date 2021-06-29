cbb5b63d81e19160de7044b684bf2d7eb7a2cc93
Ryan Smith <ryan.l.smith142@gmail.com>
2021-06-28 20:26:00 -0500

    Adds ability to run nodemon

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -120,7 +120,7 @@ app.get('/callback', function(req, res) {
         }
 
         request.get(options, function(error, response, body) {
-          console.log(body)
+          console.log(body.tracks)
         })
         // we can also pass the token to the browser to make requests from there
         res.redirect('/#' +
