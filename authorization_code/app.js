5587ebaa249a8bb14b8b8e92f604e3b39f20ce09
Ryan Smith <ryan.l.smith142@gmail.com>
2021-06-28 20:09:19 -0500

    Adds call to see objects in playlist

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -112,6 +112,16 @@ app.get('/callback', function(req, res) {
         request.get(options, function(error, response, body) {
           console.log(body)
         })
+
+        var options = {
+          url: 'https://api.spotify.com/v1/playlists/4lDWGvHAvzm7wZkMMCNe4S',
+          headers: {'Authorization': 'Bearer ' + access_token},
+          json: true
+        }
+
+        request.get(options, function(error, response, body) {
+          console.log(body)
+        })
         // we can also pass the token to the browser to make requests from there
         res.redirect('/#' +
           querystring.stringify({
